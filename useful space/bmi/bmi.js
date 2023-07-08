cad4bb88596b2fadc86a004770541f680f7cda57
Prabhat Kumar <116715572+Prabhat1137@users.noreply.github.com>
2023-07-08 10:30:25 +0530

    useful space

---
+++ useful space/bmi/bmi.js
@@ -0,0 +1,30 @@
+jQuery("#cal").click(function()
+{
+    const height=jQuery("#height").val();
+    const weight=jQuery("#weight").val();
+    const heightInMeter=height/100;
+    const bmi=(weight/(heightInMeter*heightInMeter)).toFixed(2);
+    if(bmi<18.5)
+    {
+        jQuery("#result").text("BMI is : "+bmi+", You are underweight");
+        jQuery("#result").css("color","yellow");
+        jQuery("#result").css("background-color","black");
+    }
+    else if(bmi<25){
+        jQuery("#result").text("BMI is : "+bmi+", You are healthy");
+        jQuery("#result").css("color","green");
+        // jQuery("#result").css("background-color","black");
+
+    }
+    else if(bmi<30){
+        jQuery("#result").text("BMI is : "+bmi+", You are overweight");
+        jQuery("#result").css("color","red");
+        // jQuery("#result").css("background-color","black");
+
+    }else{
+        jQuery("#result").text("BMI is : "+bmi+" You are obese");
+        jQuery("#result").css("color","red");
+    }
+    
+    
+});
